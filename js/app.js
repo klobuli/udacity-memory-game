890d62b7a3f631aefefad9fc21b1c7fbd9fb4a43
Tobias Klobuschinski <t.klobuschinski@gmail.com>
2018-11-28 18:27:36 +0100

    Create variables for each card

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const dogOne = '<i class="fas fa-dog"></i>';
+const dogTwo = '<i class="fas fa-dog"></i>';
+const catOne = '<i class="fas fa-cat"></i>';
+const catTwo = '<i class="fas fa-cat"></i>';
+const birdOne = '<i class="fas fa-crow"></i>';
+const birdTwo = '<i class="fas fa-crow"></i>';
+const hippoOne = '<i class="fas fa-hippo"></i>';
+const hippoTwo = '<i class="fas fa-hippo"></i>';
+const frogOne = '<i class="fas fa-frog"></i>';
+const frogTwo = '<i class="fas fa-frog"></i>';
+const horseOne = '<i class="fas fa-horse"></i>';
+const horseTwo = '<i class="fas fa-horse"></i>';
+const otterOne = '<i class="fas fa-otter"></i>';
+const otterTwo = '<i class="fas fa-otter"></i>';
+const spiderOne = '<i class="fas fa-spider"></i>';
+const spiderTwo = '<i class="fas fa-spider"></i>';
