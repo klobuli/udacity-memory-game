7d1df0854d79a8808989772e3531accdbca97cde
Tobias Klobuschinski <t.klobuschinski@gmail.com>
2018-12-02 13:51:26 +0100

    Rewrite functions for better functionality

--- js/app.js
+++ js/app.js
@@ -56,36 +56,37 @@ let revealCount = 0;
 let revealedAnimals = [];
 let totalMatches = 0;
 
-const card = document.querySelectorAll('.card');
-for(let i = 0; i < card.length; i++) {
-	card[i].addEventListener('click', function(evt) {
-		card[i].classList.add('open');
-		revealCount++;
-		revealedAnimals.push(card[i]);
-		if(revealCount === 2) {
-			if(revealedAnimals[0].children[0].classList[1] === revealedAnimals[1].children[0].classList[1]) {
-				const match = document.querySelectorAll('.open');
-				for(let i = 0; i < match.length; i++) {
-					setTimeout(function() {
-						match[i].classList.remove('card');
-						match[i].classList.remove('open');
-						match[i].classList.add('matched');
-					}, 1000);
-				}
-				totalMatches++;
-				revealCount = 0;
-				revealedAnimals = [];
-			}
-			else if(revealedAnimals[0].children[0].classList[1] !== revealedAnimals[1].children[0].classList[1]) {
-				const noMatch = document.querySelectorAll('.open');
-				for(let i = 0; i < noMatch.length; i++) {
-					setTimeout(function() {
-						noMatch[i].classList.remove('open');
-					}, 1000);
-				}
-				revealCount = 0;
-				revealedAnimals = [];
-			}
+function unreveal(evt) {
+	evt.currentTarget.classList.add('open');
+	revealCount++;
+	if(revealCount === 2) {
+		check();
+	}
+}
+
+function play() {
+	const card = document.querySelectorAll('.card');
+	for(let i = 0; i < card.length; i++) {
+		card[i].addEventListener('click', unreveal);
+	}
+}
+
+play();
+
+function check() {
+	const checkOpenedCards = document.querySelectorAll('.open');
+	if(checkOpenedCards[0].innerHTML === checkOpenedCards[1].innerHTML) {
+		for(let i = 0; i < checkOpenedCards.length; i++) {
+			checkOpenedCards[i].removeEventListener('click', unreveal);
+			checkOpenedCards[i].classList.remove('card', 'open');
+			checkOpenedCards[i].classList.add('matched');
 		}
-	});
+	totalMatches++;
+	}
+	else if(checkOpenedCards[0].innerHTML !== checkOpenedCards[1].innerHTML) {
+		for(let i = 0; i < checkOpenedCards.length; i++) {
+			checkOpenedCards[i].classList.remove('open');
+		}
+	}
+	revealCount = 0;
 }
